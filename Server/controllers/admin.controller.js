e03244b8d2e2ebafaf66e1342164577643f90f94
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-12-22 18:49:39 +0530

    Created a cloudinary upload function and upload the audio and file inot clodinary and save url into db

--- Server/controllers/admin.controller.js
+++ Server/controllers/admin.controller.js
@@ -1,17 +1,36 @@
 import { ApiError } from "../lib/apiError.js";
 import { asynHandler } from "../lib/asyncHandler.js";
+import cloudinary from "../lib/cloudinary.js";
 import { Album } from "../model/album.model.js";
 import { Song } from "../model/song.model.js";
 
+const uploadToCloudinary = asynHandler(async(file) => {
+    try {
+        const result = await cloudinary.uploader.upload(file.tempFilePath, {
+            resource_type: 'auto'
+        })
+        return result
+    } catch (error) {
+        console.log('Failed to upload file in cloudinary error in admincontroller.js in uploadToCloudinary function', error);
+        throw new ApiError(500, "Failed to upload file");
+    }
+})
+
+
 export const createSong = asynHandler(async (req, res) => {
     try {
-        if (!req.files || !req.files.audioFile || !req.files.imageFile) {
+        if (!req.files || !req.files.audioFile || !req.files.imageFile) {  //accepting files 
             return res.status(400).json({message: 'Please upload all files'})
         }
 
-        const {title, artist, albumId, duration} = req.body
+        const {title, artist, albumId, duration} = req.body //aceepting text 
         const audioFile = req.files.audioFile
         const imageFile = req.files.imageFile
+
+        //keeping audio files in cloudinary and then saving into db
+        const audioUrl = await uploadToCloudinary(audioFile) 
+        const imageUrl = await uploadToCloudinary(imageFile)
+
 
         const song = new Song({
             title,
@@ -39,4 +58,5 @@ export const createSong = asynHandler(async (req, res) => {
         
     }
 })
+
 
